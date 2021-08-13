207bd7aec7f61a7c0a8242ea912f8ffc53bdc131
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-12 19:54:32 -0500

    [sm] played with the github autopilot to put random images on the screen

--- dist/main.js
+++ dist/main.js
@@ -1,20 +1,30 @@
-// create an array from start to end
-const range = (start, end) => {
-  const result = [];
-  for (let i = start; i <= end; i++) {
-    result.push(i);
-  }
-  return result;
+console.log(`Hello, world!`);
+
+// Make the background of the body purple
+document.body.style.backgroundColor = 'purple';
+
+// make seven yellow squares
+for (let i = 0; i < 7; i++) {
+  const square = document.createElement('div');
+  square.style.backgroundColor = 'yellow';
+  square.style.width = '50px';
+  square.style.height = '50px';
+  document.body.appendChild(square);
 }
 
-// return every prime number in a range   
-const primes = (start, end) => {
-  const result = [];
-  for (let i = start; i <= end; i++) {
-    if (isPrime(i)) {
-      result.push(i);
-    }
-  }
-  return result;
-}
+// add a class of squares to the div    
+document.body.className = 'squares';
 
+// make the body flex   
+document.body.style.display = 'flex';
+
+// make the body flex direction row
+document.body.style.flexDirection = 'row';
+
+// add an image of a dog to the body
+const img = document.createElement('img');
+img.src = 'https://picsum.photos/600/400/?image=0';
+document.body.appendChild(img);
+
+// random number between 1 and 100
+const randomNumber = Math.floor(Math.random() * 100 + 1);
